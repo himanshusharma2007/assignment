a707e064a79eddc811fd1c0866dddc41f4248d46
himanshu sharma <sharmahimanshu6478@gmail.com>
2024-09-30 23:20:15 +0530

    testing deployment-5

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "https://products-page-bnkv.onrender.com/";
+const API_BASE_URL ="/api";
 
 export const fetchProducts = async ({
   category,
